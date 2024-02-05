168a445fb2c388f8cd9ad99bd483f6369b87d54c
10Knockout <hiteshm.ic.20@itj.ac.in>
2024-02-05 16:22:49 +0530

    recent

--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -32,15 +32,15 @@ const Edit = () => {
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
       <div className="w-[500px] h-[500px] bg-gradient-to-br from-green-400 to-blue-600 rounded-lg p-5 relative inline-flex flex-col items-center justify-center">
-        <div className="text-2xl mb-2">
-          <span className="font-bold text-3xl text-white">Name:</span> {state.name}
+        <div className="text-2xl mb-2 text-center">
+          <span className="font-bold text-3xl text-white">Name of the Package :</span> {state.name}
         </div>
-        <div className="text-2xl mt-4 mb-6">
-          <span className="font-bold text-3xl text-white">Description:</span>{" "}
+        <div className="text-2xl mt-4 mb-6 text-center">
+          <span className="font-bold text-3xl text-white">Description of the package :</span>{" "}
           {state.description}
         </div>
-        <div className="text-3xl font-bold text-white mb-4">
-          Update the Description
+        <div className="text-3xl font-bold text-white mb-4 text-center">
+          Update the Description :
         </div>
         <textarea
           value={description}
--- src/Pages/View.js
+++ src/Pages/View.js
@@ -1,18 +1,33 @@
-import { useEffect } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const View = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
 
-  useEffect(() => {
-    if (state && state.name && state.description) {
-      alert(`Name: ${state.name}\nDescription: ${state.description}`);
-      navigate("/");
-    }
-  }, [state, navigate]);
+  const handleClick = () => {
+    navigate("/");
+  };
 
-  return null;
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <div className="w-[500px] h-[500px] bg-gradient-to-br from-green-400 to-blue-600 rounded-lg p-5 relative inline-flex flex-col items-center justify-center">
+        <div className="text-xl mb-2">
+          <span className="font-bold text-2xl text-white">Name:</span> {state.name}
+        </div>
+        <div className="text-xl mb-4">
+          <span className="font-bold text-2xl text-white">Description:</span>{" "}
+          {state.description}
+        </div>
+        <button
+          className="font-medium px-2 py-1.5 transition-all ease-in duration-75 bg-black dark:bg-black rounded-md group-hover:bg-opacity-0 text-sm text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+          onClick={handleClick}
+        >
+          Return
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default View;
